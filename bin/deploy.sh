f6f8f2e46655fe9a159e77c983893fe4367c3083
Yeolar <yeolar@gmail.com>
2018-08-07 16:48:42 +0800

    fix style.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,12 +9,14 @@ HOST=$1
 
 echo "deploy to ["$1":~/app/blogger/]"
 
-rsync -av       \
-    bin         \
-    blogger     \
-    static      \
-    templates   \
-    manage.py   \
-    LICENSE     \
-    README.md   \
+rsync -av                   \
+    --exclude=settings.py   \
+    --exclude=*.pyc         \
+    bin                     \
+    blogger                 \
+    static                  \
+    templates               \
+    manage.py               \
+    LICENSE                 \
+    README.md               \
     $HOST:~/app/blogger/
